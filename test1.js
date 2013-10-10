aa5146326ac0e0609d28d665f1ced683ffa8075b
cssrain <cssrain@gmail.com>
2013-10-10 12:51:37 +0800

    修改test1.js

--- test1.js
+++ test1.js
@@ -1,3 +1,9 @@
 test("第一个JavaScript单元测试", function() {
 	ok( 1 == "1", "Passed!" );
 });
+/*
+test("第二个JavaScript单元测试", function() {
+ 	var value = "hello";
+ 	equal( value, "hello", "Passed!" );
+});
+*/
